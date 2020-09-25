7b56c9a7f6de8fe4e5a37f9edc1f81cae9eb9624
ikeday <ikeday819@gmail.com>
2020-09-25 13:10:22 +0900

    initial committe

---
+++ iFrame2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+<META HTTP-EQUIV="Expires" CONTENT="-1">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="../taskevents/js/jquery.quicksearch.min.js" type="text/javascript"></script>
+<title>Menu</title>
+</head>
+<body>
+<div id="thirdtext">Third Text</div>
+<center><table width="580px" border = "1" id="eventTable">
+    <tr><th width="40px">No</th><th width="40px">機関</th><th width="250px">プロジェクト</th><th width="250px">詳細</th></tr>
+    <tbody id="eventBody">
+    <tr id="row1" bgcolor="#FFFFFF"><td id="No" align = "center">1</td><td id="nb_td">BSI</td><td id="project_td"><a href="./displayEventTable.php?nb=BSI&project=2020%E5%B9%B4%E7%9B%A3%E6%9F%BB" target="contents">2020年監査</a></td><td id="detail_td">CE＆AU/NZS認証監査(2020)</td></tr>
+    <tr id="row2" bgcolor="#C0C0C0"><td id="No" align = "center">2</td><td id="nb_td">BSI</td><td id="project_td"><a href="./displayEventTable.php?nb=BSI&project=FS01%E3%81%AE%E9%83%A8%E5%93%81%E5%BD%A2%E7%8A%B6%E5%A4%89%E6%9B%B4" target="contents">FS01の部品形状変更</a></td><td id="detail_td">フロントパネルBの形状変更</td></tr>
+    <tr id="row3" bgcolor="#FFFFFF"><td id="No" align = "center">3</td><td id="nb_td">BSI</td><td id="project_td"><a href="./displayEventTable.php?nb=BSI&project=P2RC%2FP3RC%E3%81%AE%E8%AA%8D%E8%A8%BC" target="contents">P2RC/P3RCの認証</a></td><td id="detail_td">製品認証</td></tr>
+    <tr id="row4" bgcolor="#C0C0C0"><td id="No" align = "center">4</td><td id="nb_td">BSI</td><td id="project_td"><a href="./displayEventTable.php?nb=BSI&project=P2RC%E3%81%AEOBL%E8%AA%8D%E8%A8%BC" target="contents">P2RCのOBL認証</a></td><td id="detail_td">OEM製品認証(Arco)</td></tr>
+    <tr id="row5" bgcolor="#FFFFFF"><td id="No" align = "center">5</td><td id="nb_td">BSI</td><td id="project_td"><a href="./displayEventTable.php?nb=BSI&project=PAPR%E3%81%AE%E9%9B%BB%E6%B0%97%E7%9A%84%E5%AE%89%E5%85%A8%E6%80%A7" target="contents">PAPRの電気的安全性</a></td><td id="detail_td">EMC関連</td></tr>
+    <tr id="row6" bgcolor="#C0C0C0"><td id="No" align = "center">6</td><td id="nb_td">BSI</td><td id="project_td"><a href="./displayEventTable.php?nb=BSI&project=Sync09%2F11V1B2E2K1P3%E3%81%AE%E8%A9%A6%E9%A8%93" target="contents">Sync09/11V1B2E2K1P3の試験</a></td><td id="detail_td">試験及び認証</td></tr>
+    <tr id="row7" bgcolor="#FFFFFF"><td id="No" align = "center">7</td><td id="nb_td">BSI</td><td id="project_td"><a href="./displayEventTable.php?nb=BSI&project=TS01%2BYP3R%E3%83%95%E3%82%A3%E3%83%AB%E3%82%BF%E3%81%AE%E8%AA%8D%E8%A8%BC" target="contents">TS01+YP3Rフィルタの認証</a></td><td id="detail_td">試験及び認証</td></tr>
+    <tr id="row8" bgcolor="#C0C0C0"><td id="No" align = "center">8</td><td id="nb_td">BSI</td><td id="project_td"><a href="./displayEventTable.php?nb=BSI&project=TW08%E8%AA%8D%E8%A8%BC" target="contents">TW08認証</a></td><td id="detail_td">CE&AU/NZ認証（試験込み）</td></tr>
+	</tbody>
+</table>
+</center>
+</body>
+</html>
